4dcfe2c62d54e93a9ef588f5ec9b846c7972abd9
Nick Schmitz <nick.d.schmitz@gmail.com>
2022-10-28 10:23:37 -0500

    3 days of code wars

---
+++ 8 kyu/Opposite number.js
@@ -0,0 +1,10 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+
+function opposite(number) {
+  return number * (-1);
+}
+
+function opposite(number) {
+  return(-number);
+}
