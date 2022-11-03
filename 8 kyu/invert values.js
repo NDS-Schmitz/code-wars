6e461549a30e6c239df767bbd337b8881d13f12c
Nick Schmitz <nick.d.schmitz@gmail.com>
2022-11-03 18:15:47 -0500

    daily code wars

---
+++ 8 kyu/invert values.js
@@ -0,0 +1,13 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+
+function invert(array) {
+    return array.map(x => x * -1)        
+    }
+
+    //other solution
+
+    const invert = array => array.map(num => -num);
