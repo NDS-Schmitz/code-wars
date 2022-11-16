bd36820798752dcfc91c11440b4e408916bb3d55
Nick Schmitz <nick.d.schmitz@gmail.com>
2022-11-15 20:17:57 -0600

    code war

---
+++ 8 kyu/Training Arrays .js
@@ -0,0 +1,25 @@
+// In javascript, Array is one of basic data types. To define an empty array, you can use var arr=new Array() or var arr=[]
+
+// Array has an attribute: length, if there is an array named arr, using arr.length to know how many elements are contained in the array.
+
+// Each element in the array has an index, use arr[index] to get the value of element.
+
+// index always start from 0, so the first element of array is arr[0], the last element of array is arr[arr.length-1].
+
+function getLength(arr) {
+  return arr.length;
+}
+function getFirst(arr) {
+  return arr[0];
+}
+function getLast(arr) {
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  arr.push(1);
+  return arr;
+}
+function popElement(arr) {
+  arr.pop();
+  return arr;
+}
