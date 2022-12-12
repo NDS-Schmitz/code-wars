08eeb8a756ff9fdee54424cbf9673c3c815de0d0
Nick Schmitz <nick.d.schmitz@gmail.com>
2022-12-12 17:26:43 -0600

    Code Wars

---
+++ 5 kyu/directions reduction.js
@@ -0,0 +1,21 @@
+// Once upon a time, on a way through the old wild mountainous west,…
+// … a man was given directions to go from one point to another. The directions were "NORTH", "SOUTH", "WEST", "EAST". Clearly "NORTH" and "SOUTH" are opposite, "WEST" and "EAST" too.
+
+// Going to one direction and coming back the opposite direction right away is a needless effort. Since this is the wild west, with dreadful weather and not much water, it's important to save yourself some energy, otherwise you might die of thirst!
+
+function dirReduc(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      (arr[i] === "WEST" && arr[i + 1] === "EAST") ||
+      (arr[i] === "EAST" && arr[i + 1] === "WEST") ||
+      (arr[i] === "NORTH" && arr[i + 1] === "SOUTH") ||
+      (arr[i] === "SOUTH" && arr[i + 1] === "NORTH")
+    ) {
+      arr.splice(i, 2);
+      count++;
+      i--;
+    }
+  }
+  return count === 0 ? arr : dirReduc(arr);
+}
